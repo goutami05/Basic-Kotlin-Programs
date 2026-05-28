9d795da1014545bb28f51e6756be035c30dc8144
Goutami Sahu <goutamisahu05@gmail.com>
2026-05-28 14:31:51 +0530

    Implement loop to print numbers from 1 to 10

---
+++ q1.kt
@@ -0,0 +1,10 @@
+/**
+ * Question 1: Print numbers from 1 to 10 using a loop.
+ */
+fun main() {
+    println("Numbers from 1 to 10:")
+    for (i in 1..10) {
+        print("$i ")
+    }
+    println()
+}
