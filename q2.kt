d1096754096ddaf667ed129454ea9144edf550ac
Goutami Sahu <goutamisahu05@gmail.com>
2026-05-28 14:33:30 +0530

    Add Kotlin program to check even or odd number

---
+++ q2.kt
@@ -0,0 +1,11 @@
+/**
+ * Question 2: Check whether a given number is even or odd.
+ */
+fun main() {
+    val number = 27
+    if (number % 2 == 0) {
+        println("$number is an Even number.")
+    } else {
+        println("$number is an Odd number.")
+    }
+}
